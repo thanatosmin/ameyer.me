e5dfcfac42f7f2ef1bb3a810234caf9d399ba7bf
Aaron Meyer <a@ameyer.me>
2017-01-21 16:15:59 -0500

    Thanatosmin patch 1 (#1)
    A bunch of edits to make building work through Travis CI.
    * Create README.md
    * HTML proofing and complete move to Travis CI
    * Make sure html proofer is installed, and put build in README.
    * Remove spaces in generated archive links.
    * Missed one.
    * Removing two other HTML errors.
    * Date padding tweak.
    * Reorganized scripts.

---
+++ README.md
@@ -0,0 +1,3 @@
+# [Personal website of Aaron Meyer](http://ameyer.me)
+
+[![Build Status](https://travis-ci.org/thanatosmin/ameyer.me.svg)](https://travis-ci.org/thanatosmin/ameyer.me)
---
+++ scripts/htmlproof.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+require 'html-proofer'
+
+HTMLProofer.check_directory('./_site', {
+	:parallel => {
+		:in_processes => 4,
+	},
+	:check_html => false,
+	:cache => {
+		:timeframe => '1w',
+	},
+	:url_ignore => [/scholar.google.com/, /linkedin.com/, /ng.2330.html/],
+	:typhoeus => {
+		:timeout => 15, # seconds
+	},
+	:verbose => true,
+}).run
