2b1c844eaf4f50ebacb59ab9db4bc0bc4d70af32
Aaron Meyer <a@ameyer.me>
2017-05-22 08:20:52 -0400

    Update htmlproof.rb

--- scripts/htmlproof.rb
+++ scripts/htmlproof.rb
@@ -8,9 +8,8 @@ HTMLProofer.check_directory('./_site', {
 	},
 	:check_html => false,
 	:cache => {
-		:timeframe => '1w',
-	},
-	:url_ignore => [/scholar.google.com/, /linkedin.com/, /ng.2330.html/],
+		:timeframe => '12M',
+	}, #:url_ignore => [/scholar.google.com/, /linkedin.com/, /ng.2330.html/],
 	:typhoeus => {
 		:timeout => 15, # seconds
 	},
