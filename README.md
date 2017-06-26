0656450f1f197756aaa6fba5a33b4b1d79725744
Aaron Meyer <a@ameyer.me>
2017-06-25 17:48:51 -0700

    Move over to Jenkins.

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # [Personal website of Aaron Meyer](http://ameyer.me)
-
-[![Build Status](https://travis-ci.org/thanatosmin/ameyer.me.svg)](https://travis-ci.org/thanatosmin/ameyer.me)
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,28 +2,6 @@
 
 set -o errexit -o nounset
 
-if [ -f "_config.yml" ]
-then
-	echo "Starting build."
-else
-	echo "Are you in the right directory?"
-  exit 1
-fi
-
-### Start push
-
-if [ "$TRAVIS_BRANCH" != "master" ]
-then
-  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
-  exit 0
-fi
-
-if [ "$TRAVIS_PULL_REQUEST" = "true" ]
-then
-  echo "This is a pull request! No deploy!"
-  exit 0
-fi
-
 # Deploy to site
 
 lftp -c "set ftp:list-options -a;
@@ -37,16 +15,17 @@ mirror --reverse --delete --verbose \
 cd _site
 
 git init
-git config user.name "Travis CI"
-git config user.email "notifications@travis-ci.org"
+git config user.name "Jenkins CI"
+git config user.email "jenkins@asmlab.org"
 
-git remote add upstream "https://$GH_TOKEN@github.com/thanatosmin/ameyer.me.git"
+
+git remote add upstream "git@github.com:thanatosmin/ameyer.me.git"
 git fetch upstream
-git reset upstream/built-site
+git reset upstream/gh-pages
 
 touch .
 
 git add -A .
 git status
-git commit -m "Lastest site built on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to github"
-git push -q upstream HEAD:built-site
+git commit -m "Lastest site built on successful travis build $BUILD_ID auto-pushed to github"
+git push -q upstream HEAD:gh-pages
